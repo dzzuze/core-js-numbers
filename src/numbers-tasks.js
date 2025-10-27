0af79e7c21fb04caf8b2b985a309f268e4e59f20
dzzuze <notinsidik@gmail.com>
2025-10-28 00:02:06 +0300

    feat: implement isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -211,8 +211,23 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  if (n % 2 === 0) {
+    return false;
+  }
+  const limit = Math.sqrt(n);
+  for (let i = 3; i <= limit; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
